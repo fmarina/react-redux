cc1ce9db0ebcfc0ef72a441f994afb218b968510
marinafaenze <marinafaenze@hotmail.com>
2022-10-19 11:30:47 -0300

    OMF-121: configureStore, implement redux persis, create routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import StepCounter from "./exercise26";
+import ItemsList from "./exercise27";
+import ShoppingList from "./exercise27extra";
+import ShoppingListDetail from "./exercise27extra/ShoppingListDetail";
+import LoginApp from "./exercise28";
+import Dashboard from "./exercise28/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/exercise26">Exercise 26: Step Counter</Link>
+              </li>
+              <li>
+                <Link to="/exercise27">Exercise 27: Shopping List</Link>
+              </li>
+              <li>
+                <Link to="/exercise27/extra">Exercise 27: Extra</Link>
+              </li>
+              <li>
+                <Link to="/exercise28">
+                  Exercise 28: Login & Exercise 29: Login Persist
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/exercise26" element={<StepCounter />} />
+          <Route path="/exercise27" element={<ItemsList />} />
+          <Route exact path="/exercise27/extra" element={<ShoppingList />} />
+          <Route path="/exercise27/extra/:id" element={<ShoppingListDetail />} />
+          <Route path="/exercise28" element={<LoginApp />} />
+          <Route path="/exercise28/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
+import rootReducer from './reducers'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+const persistor = persistStore(store)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />      
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
